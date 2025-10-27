530e4d6f1c9342259cc6e2553d56d215eaa3ea2f
levisq <leviqueroz@hotmail.com>
2025-10-27 14:37:01 -0300

    finalizando endpoint de status

--- app/api/v1/status/route.js
+++ app/api/v1/status/route.js
@@ -1,10 +1,22 @@
 import database from "infra/database.js"
 
-export async function GET() {
-  const result = await database.query('SELECT 1 + 1 as sum;');
-  console.log(result.rows);
-  return new Response(JSON.stringify({ status: "ok", message: "TO AQUI" }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+export async function GET(request) {
+  const datBaseName = process.env.POSTGRES_DB;
+  const updateAt = new Date().toISOString();
+  const dbVersion = (await database.query(
+    "SELECT Version();")).rows[0].version;
+  const dbMaxConnect = (await database.query(
+    "SHOW max_connections;")).rows[0].max_connections;
+  const dbUsedConnections = (await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [datBaseName],
+  })).rows[0].count;
+  console.log(dbUsedConnections)
+  return new Response(
+    JSON.stringify({ update_at: updateAt, db_version: dbVersion, db_max_connections: dbMaxConnect, db_used_connections: dbUsedConnections}),
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    }
+  )
 }
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -1,4 +1,27 @@
+//test.only vc roda apenas o test que foi colocado o .only
+
 test("GET to api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   expect(response.status).toBe(200);
-});
+
+  const responseBody = await response.json();
+  expect(responseBody.update_at).toBeDefined();
+  
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString();
+  expect(responseBody.update_at).toEqual(parsedUpdateAt)
+
+  //version
+  const dbVersion = responseBody.db_version;
+  expect(dbVersion).toEqual('PostgreSQL 18.0 on x86_64-pc-linux-musl, compiled by gcc (Alpine 14.2.0) 14.2.0, 64-bit')
+  expect(dbVersion).not.toBeNull()
+
+  //maxconnect
+  const dbMaxConnect = parseInt(responseBody.db_max_connections, 10);
+  expect(dbMaxConnect).toEqual(100);
+  expect(dbMaxConnect).not.toBeNull()
+
+  //usedconnection
+  const dbUsedConnections = responseBody.db_used_connections;
+  expect(dbUsedConnections).toBeLessThan(parseInt(dbMaxConnect, 10))
+  expect(dbUsedConnections).not.toBeNull()
+})
